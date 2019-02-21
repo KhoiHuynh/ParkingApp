df312b238a68337f32b6308962b9ca87f7fd386f
Khoi <khuynh09@hotmail.com>
2019-02-20 22:56:58 -0500

    linting

--- app/src/main/java/com/example/khoi/parkingapp/fragments/AddLocationFragment.kt
+++ app/src/main/java/com/example/khoi/parkingapp/fragments/AddLocationFragment.kt
@@ -1,8 +1,6 @@
 package com.example.khoi.parkingapp.fragments
 
-
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,9 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.example.khoi.parkingapp.R
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment
-
 
 class AddLocationFragment : BaseFragment() {
     companion object {
@@ -32,41 +28,25 @@ class AddLocationFragment : BaseFragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add_location, container, false)
-//        if (activity?.fragmentManager != null) {
-//            val autocompleteFragment2 =
-//                activity?.fragmentManager?.findFragmentById(R.id.place_autocomplete_fragment2) as PlaceAutocompleteFragment
-//            autocompleteFragment2.setHint("Enter you parking spot address")
         val fm: FragmentManager = childFragmentManager
-//        var placeAutocompleteFragment: PlaceAutocompleteFragment? = activity!!.supportFragmentManager.findFragmentByTag("placeAutocompleteFragment") as PlaceAutocompleteFragment?
         var placeAutocompleteFragment: SupportPlaceAutocompleteFragment? = fm.findFragmentByTag("placeAutocompleteFragment") as SupportPlaceAutocompleteFragment?
+
         if (placeAutocompleteFragment == null){
             placeAutocompleteFragment = SupportPlaceAutocompleteFragment()
             fm.beginTransaction().add(R.id.address_layout, placeAutocompleteFragment, "placeAutocompleteFragment").commit()
             fm.executePendingTransactions()
         }
-
+        placeAutocompleteFragment.setHint("Enter spot address")
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button = view.findViewById(R.id.button_next) as Button
-//        val fm: FragmentManager = childFragmentManager
-////        var placeAutocompleteFragment: PlaceAutocompleteFragment? = activity!!.supportFragmentManager.findFragmentByTag("placeAutocompleteFragment") as PlaceAutocompleteFragment?
-//        var placeAutocompleteFragment: PlaceAutocompleteFragment? = fm.findFragmentByTag("placeAutocompleteFragment") as PlaceAutocompleteFragment?
-//
-//        if (placeAutocompleteFragment == null){
-//            placeAutocompleteFragment = PlaceAutocompleteFragment()
-//            fm.beginTransaction().add(R.id.address_layout, placeAutocompleteFragment, "placeAutocompleteFragment").commit()
-//            fm.executePendingTransactions()
-//        }
-
 
         button.setOnClickListener{
             Log.d(TAG, "Clicked")
             mFragmentNavigation.pushFragment(Host2Fragment.newInstance(0))
         }
     }
-
-
 }
