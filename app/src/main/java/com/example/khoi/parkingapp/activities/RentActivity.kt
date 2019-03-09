f55a26c65928f86a2de86ad5f9b1960d125faeea
Khoi <khuynh09@hotmail.com>
2019-03-09 01:16:16 -0500

    completed Range Seekbar with the dynamic textView displaying the time

--- app/src/main/java/com/example/khoi/parkingapp/activities/RentActivity.kt
+++ app/src/main/java/com/example/khoi/parkingapp/activities/RentActivity.kt
@@ -6,21 +6,27 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarFinalValueListener
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar
 import com.example.khoi.parkingapp.R
 import kotlinx.android.synthetic.main.activity_rent.*
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.collections.ArrayList
 
-private var address: String? = null
-private var description: String? = null
-private var rate: String? = null
-private var days: ArrayList<Int>? = null
-private var fromTime: String? = null
-private var toTime: String? = null
-
 class RentActivity : AppCompatActivity(), View.OnClickListener {
+    private var address: String? = null
+    private var description: String? = null
+    private var rate: String? = null
+    private var days: ArrayList<Int>? = null
+    private var fromTime: String? = null
+    private var toTime: String? = null
+
+    companion object {
+        private const val TAG = "RentActivity"
+    }
+
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.toggleButton_monday ->{
@@ -35,12 +41,49 @@ class RentActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    companion object {
-        private const val TAG = "RentActivity"
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rent)
+        setupUI()
+
+    }
+
+    private fun convertStringTimeToMin(strTime: String): Int{
+        var tempStrTime = strTime
+        val zone = strTime.takeLast(2)
+        tempStrTime = tempStrTime.substring(0, tempStrTime.length-2).trimEnd()
+        val hours = tempStrTime.substringBefore(":").toInt()
+        val minutes = tempStrTime.substringAfter(":").toInt()
+        var totalMinutes = (hours * 60) + (minutes)
+
+        if(zone == "AM"){
+            totalMinutes -= 720
+        }
+
+        return totalMinutes
+    }
+
+    private fun convertMinToStrTime(minutes: Int): String{
+        var zone = "AM"
+        var hours = minutes / 60
+        var mins: String = ""
+
+        if((minutes%60) < 10){
+            mins += "0${minutes%60}"
+        }
+        else{
+            mins += (minutes%60)
+        }
+
+        if(hours > 12){
+            hours %= 12
+            zone = "PM"
+        }
+
+        return "$hours:$mins $zone"
+    }
+
+    private fun setupUI(){
         val intent: Intent = intent
         val bundle: Bundle = intent.extras?.get("bundle") as Bundle
         address = bundle.getString("address")
@@ -71,10 +114,32 @@ class RentActivity : AppCompatActivity(), View.OnClickListener {
         toggleButton_saturday.setOnClickListener(this)
         toggleButton_saturday.setOnClickListener(this)
 
+        //grey out and disable day buttons that aren't available
         greyOutButtons(days!!)
 
-        Log.d(TAG, "days: " + days)
+        //range seekbar
+        val earliestTime = convertStringTimeToMin(fromTime!!)
+        val latestTime = convertStringTimeToMin(toTime!!)
+        rangeSeekbar.setMinValue(earliestTime.toFloat())
+        rangeSeekbar.setMaxValue(latestTime.toFloat())
 
+        // we want a gap of 15 minutes minimum, so we need to find the percentage of the time different of the
+        // latestTime minus earliestTime.
+        //ex: the time difference between latest and earliest is 60min
+        //    we want a 15minute gap, so 15min of 60min is 25%. 25 is the number we use to set the gap
+        val timeDiff: Float = (latestTime - earliestTime).toFloat()
+        val gapInMinutes = 15.0f
+        val gapPercentage: Float = (gapInMinutes/timeDiff)*100
+        rangeSeekbar.setGap(gapPercentage)
+        rangeSeekbar.setOnRangeSeekbarChangeListener { minValue, maxValue ->
+
+        val timeRange = "${convertMinToStrTime(minValue.toInt())} - ${convertMinToStrTime(maxValue.toInt())}"
+        tv_rent_time_range.text = timeRange
+        }
+
+        rangeSeekbar.setOnRangeSeekbarFinalValueListener { minValue, maxValue ->
+            println("User is renting from: ${convertMinToStrTime(minValue.toInt())} until ${convertMinToStrTime(maxValue.toInt())}")
+        }
     }
 
     private fun greyOutButtons(days: ArrayList<Int>){
